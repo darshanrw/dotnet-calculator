7a1689967cf84f6b96d90700533e2ad08a5b036a
Darshan Rajendra Wadekar <Dwadekar4575@conestogac.on.ca>
2024-06-06 19:13:14 -0400

    Commiting final changes!

--- Calculator.Console/Program.cs
+++ Calculator.Console/Program.cs
@@ -1,33 +1,44 @@
 ﻿using Calculator;
 
 Prompts.PrintWelcomeMenu();
-Prompts.PrintOptions();
-Console.WriteLine("Enter operation number: ");
-string? OptionChoice = Console.ReadLine();
-Console.WriteLine("Enter number 1: ");
-string? Number1 = Console.ReadLine();
-Console.WriteLine("Enter number 2: ");
-string? Number2 = Console.ReadLine();
+while (true)
+{
 
-float Number1Converted = float.Parse(Number1);
-float Number2Converted = float.Parse(Number2);
-switch(OptionChoice){
-    case "1":
-        float Sum = Evaluator.Eval("+", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} + {Number2Converted} = {Sum}");
-        break;
-    case "2":
-        float Subtract = Evaluator.Eval("-", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} - {Number2Converted} = {Subtract}");
-        break;
-    case "3":
-        float Product = Evaluator.Eval("*", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} * {Number2Converted} = {Product}");
-        break;
-    case "4":
-        float Divide = Evaluator.Eval("/", Number1Converted, Number2Converted);
-        Console.WriteLine($"{Number1Converted} / {Number2Converted} = {Divide}");
-        break;
-    default:
-        throw new Exception("unimplemented");
+
+    Prompts.PrintOptions();
+    Console.WriteLine("Enter operation number: ");
+    string? OptionChoice = Console.ReadLine();
+    if(OptionChoice.Equals("5"))
+    {
+        Console.WriteLine("Exit Peacefully");
+        Environment.Exit(0);
+    }
+    Console.WriteLine("Enter number 1: ");
+    string? Number1 = Console.ReadLine();
+    Console.WriteLine("Enter number 2: ");
+    string? Number2 = Console.ReadLine();
+
+    float Number1Converted = float.Parse(Number1);
+    float Number2Converted = float.Parse(Number2);
+    switch (OptionChoice)
+    {
+        case "1":
+            float Sum = Evaluator.Eval("+", Number1Converted, Number2Converted);
+            Console.WriteLine($"{Number1Converted} + {Number2Converted} = {Sum}");
+            break;
+        case "2":
+            float Subtract = Evaluator.Eval("-", Number1Converted, Number2Converted);
+            Console.WriteLine($"{Number1Converted} - {Number2Converted} = {Subtract}");
+            break;
+        case "3":
+            float Product = Evaluator.Eval("*", Number1Converted, Number2Converted);
+            Console.WriteLine($"{Number1Converted} * {Number2Converted} = {Product}");
+            break;
+        case "4":
+            float Divide = Evaluator.Eval("/", Number1Converted, Number2Converted);
+            Console.WriteLine($"{Number1Converted} / {Number2Converted} = {Divide}");
+            break;
+        default:
+            throw new Exception("unimplemented");
+    }
 }
